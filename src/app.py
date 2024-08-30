27122faa972387244750b892f9803d2b749b255e
imiebaka <triple07even@gmail.com>
2024-08-30 02:45:21 +0100

    🛠️ Updated app.py to use POST request with form data, added Payload model, modified bot_response function, updated style.css with new styles, and updated index.html with new layout and functionality.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body, Form
 from starlette.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from src.utils.bot import bot_response
-
+from pydantic import BaseModel, Field
+from typing import Annotated
 app = FastAPI()
 
 app.add_middleware(
@@ -28,10 +29,17 @@ async def root(request: Request):
     )
 
 
-@app.get("/request")
-async def make_request():
+
+class Payload(BaseModel):
+    prompt: str = Field(
+        default=None, title="Enter a prompt", max_length=300
+    )
+
+@app.post("/request")
+async def make_request(prompt: str = Form(...)):
     try:
-        res = bot_response()
+        print(prompt)
+        res = bot_response(prompt=prompt)
         return {"message": res}
     except Exception as ex:
         raise HTTPException(status_code=400, detail=str(ex))
--- src/utils/bot.py
+++ src/utils/bot.py
@@ -3,10 +3,10 @@ from langchain_openai import ChatOpenAI, OpenAI
 from langchain_core.messages import HumanMessage, AIMessage
 import openai
 
-def bot_response():
+def bot_response(prompt: str):
     try:
         model = ChatOpenAI(model="gpt-3.5-turbo")
-        res = model.invoke([HumanMessage(content="Hi! I'm Bob")])
+        res = model.invoke([HumanMessage(content=f"translate this to english {prompt}")])
         return res.content
     except openai.RateLimitError:
         raise Exception("Rate limit exceeded. Please try again later.")
